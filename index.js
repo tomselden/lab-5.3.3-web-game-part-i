93113427af71b28182ebe255b95686c64f02366c
Thomas Selden <thomasselden42@gmail.com>
2021-10-29 20:49:07 -0400

    submission for web game

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+
+function newImage(spriteAsset, spriteLeftValue, spriteBottomValue){
+
+    let newSprite = document.createElement('img')
+    newSprite.src = spriteAsset
+    newSprite.style.position = 'fixed'
+    newSprite.style.left = spriteLeftValue
+    newSprite.style.bottom = spriteBottomValue
+    document.body.append(newSprite)
+
+}
+
+function newItem(spriteAsset, spriteLeftValue, spriteBottomValue){
+
+    let newSprite = document.createElement('img')
+    newSprite.src = spriteAsset
+    newSprite.style.position = 'fixed'
+    newSprite.style.left = spriteLeftValue
+    newSprite.style.bottom = spriteBottomValue
+    document.body.append(newSprite)
+    newSprite.addEventListener('dblclick', function(){
+        newSprite.remove()
+    })
+
+}
+
+
+    let greenCharacter = newImage('assets/green-character.gif', '100px', '100px')
+    let pineTree = newImage('assets/pine-tree.png', '450px', '200px')
+    let tree = newImage('assets/tree.png', '200px', '300px')
+    let pillar = newImage('assets/pillar.png', '350px', '100px')
+    let crate = newImage('assets/crate.png', '150px', '200px')
+    let well = newImage('assets/well.png', '500px', '425px')
+
+    let swordItem = newItem('assets/sword.png', '500px', '405px')
+    let shieldItem = newItem('assets/sheild.png', '165px', '600px')
+    let staffItem = newItem('assets/staff.png', '600px', '100px')
+
+
+
+
